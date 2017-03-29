2520f36d1f6df5828d1ab6d84bc986ce0be12eba
Atsushi TAKEDA <atsushi@takedarts.jp>
2017-03-29 22:29:44 +0900

    change path specification of makefiles from absolute to relative

--- util/config.pl
+++ util/config.pl
@@ -9,27 +9,30 @@
 #   APPL_COBJS: 生成するオブジェクトファイル(C)
 #   APPL_CXXOBJS:  生成するオブジェクトファイル(C++)
 #
+use File::Spec;
 
 sub make_makefile_inc {
-  my ($src, $dst, $src_dir, $c_objs, $cpp_objs) = @_;
+  my ($src, $dst, $src_dir, $obj_dir, $c_objs, $cpp_objs) = @_;
   my $in, $out;
   my @c_objs = @$c_objs;
   my @cpp_objs = @$cpp_objs;
   my $c_objs_str = join(' ', @c_objs);
   my $cpp_objs_str = join(' ', @cpp_objs);
-  
+
   if(!open($in, $src)){
     die "can't open file: $src";
   }
-  
+
   if(!open($out, ">$dst")){
     die "can't open file: $dst";
   }
-  
+
   while(<$in>){
     print $out $_;
   }
-  
+
+  $src_dir = File::Spec->abs2rel($src_dir, $obj_dir);
+
   print $out "# ----- added by config.pl -----\r\n";
   print $out "MOD_SRCDIR := $src_dir\r\n";
   print $out "MOD_COBJS := $c_objs_str\r\n";
@@ -40,17 +43,20 @@ sub make_makefile_inc {
 }
 
 sub make_makefile_app {
-  my ($src, $dst, $src_dir, $ev3rt_dir) = @_;
+  my ($src, $dst, $src_dir, $obj_dir, $ev3rt_dir) = @_;
   my $in, $out;
 
   if(!open($in, $src)){
     die "can't open file: $src";
   }
-  
+
   if(!open($out, ">$dst")){
     die "can't open file: $dst";
   }
-  
+
+  $src_dir = File::Spec->abs2rel($src_dir, $obj_dir);
+  $ev3rt_dir = File::Spec->abs2rel($ev3rt_dir, $obj_dir);
+
   while(<$in>){
     ~s/KERNELDIR = \@\(SRCDIR\)/KERNELDIR = ${ev3rt_dir}/;
     ~s/APPLDIR   = \@\(APPLDIR\)/APPLDIR   = ${src_dir}/;
@@ -65,7 +71,7 @@ sub make_makefile_app {
     ~s/include \$\(LOADERDIR\)\/app\/Makefile.lum/include Makefile.lum/;
     print $out $_;
   }
-  
+
   close($in);
   close($out);
 }
@@ -77,42 +83,44 @@ sub make_makefile_lum {
   if(!open($in, $src)){
     die "can't open file: $src";
   }
-  
+
   if(!open($add, "$tmp")){
     die "can't open file: $tmp";
   }
-  
+
   if(!open($out, ">$dst")){
     die "can't open file: $dst";
   }
-  
+
   while(<$in>){
-    ~s/(\$\(OBJFILE\): \$\(CFG_OUT_SRCS\) \$\(ALL_OBJS\) \$\(ALL_CXXOBJS\))/$1 \$\(MOD_COBJS\) \$\(MOD_CXXOBJS\)/; 
-    ~s/(\@\$\(LINK\) \$\(LDFLAGS\) -o \$\(OBJFILE\) \$\(ALL_OBJS\) \$\(ALL_CXXOBJS\))/$1 \$\(MOD_COBJS\) \$\(MOD_CXXOBJS\)/; 
+    ~s/(\$\(OBJFILE\): \$\(CFG_OUT_SRCS\) \$\(ALL_OBJS\) \$\(ALL_CXXOBJS\))/$1 \$\(MOD_COBJS\) \$\(MOD_CXXOBJS\)/;
+    ~s/(\@\$\(LINK\) \$\(LDFLAGS\) -o \$\(OBJFILE\) \$\(ALL_OBJS\) \$\(ALL_CXXOBJS\))/$1 \$\(MOD_COBJS\) \$\(MOD_CXXOBJS\)/;
     print $out $_;
   }
-  
+
   while(<$add>){
     print $out $_;
   }
-  
+
   close($in);
   close($add);
   close($out);
 }
 
 sub make_makefile_prj {
-  my ($src, $dst, $ev3rt_dir) = @_;
+  my ($src, $dst, $obj_dir, $ev3rt_dir) = @_;
   my $in, $out;
 
   if(!open($in, $src)){
     die "can't open file: $src";
   }
-  
+
   if(!open($out, ">$dst")){
     die "can't open file: $dst";
   }
-  
+
+  $ev3rt_dir = File::Spec->abs2rel($ev3rt_dir, $obj_dir);
+
   while(<$in>){
     ~s/EV3RT_SDK_COM_DIR := \$\(.+\)/EV3RT_SDK_COM_DIR := ${ev3rt_dir}\/sdk\/common/g;
     ~s/EV3RT_SDK_API_DIR := \$\(.+\)/EV3RT_SDK_API_DIR := ${ev3rt_dir}\/sdk\/common\/ev3api/;
@@ -121,7 +129,7 @@ sub make_makefile_prj {
     ~s/(APPL_DIR \+= \$\(foreach)/# $1/;
     print $out $_;
   }
-  
+
   close($in);
   close($out);
 }
@@ -131,25 +139,25 @@ sub make_cfgfile {
   my $in, $out;
   my @c_objs = @$c_objs;
   my @cpp_objs = @$cpp_objs;
-  
+
   if(!open($in, $src)){
     die "can't open file: $src";
   }
-  
+
   if(!open($out, ">$dst")){
     die "can't open file: $dst";
   }
-  
+
   while(<$in>){
     print $out $_;
   }
-  
+
   print $out "\r\n/* ----- added by config.pl ----- */\r\n";
-  
+
   foreach(@c_objs){
     print $out "ATT_MOD(\"$_\");\r\n";
   }
-  
+
   foreach(@cpp_objs){
     print $out "ATT_MOD(\"$_\");\r\n";
   }
@@ -169,7 +177,7 @@ sub run {
 
   my @c_objs = grep $_ ne '', split(/\s+/, $c_objs);
   my @cpp_objs = grep $_ ne '', split(/\s+/, $cpp_objs);
-  
+
   my $makefile_inc_src = "${res_dir}/Makefile.inc";
   my $makefile_inc_dst = "${obj_dir}/Makefile.inc";
   my $makefile_app_src = "${ev3rt_dir}/sdk/common/Makefile.app";
@@ -181,11 +189,11 @@ sub run {
   my $makefile_prj_dst = "${obj_dir}/Makefile.prj";
   my $cfgfile_src = "${res_dir}/app.cfg";
   my $cfgfile_dst = "${obj_dir}/app.cfg";
-  
-  make_makefile_inc($makefile_inc_src, $makefile_inc_dst, $src_dir, \@c_objs, \@cpp_objs);
-  make_makefile_app($makefile_app_src, $makefile_app_dst, $src_dir, $ev3rt_dir);
+
+  make_makefile_inc($makefile_inc_src, $makefile_inc_dst, $src_dir, $obj_dir, \@c_objs, \@cpp_objs);
+  make_makefile_app($makefile_app_src, $makefile_app_dst, $src_dir, $obj_dir, $ev3rt_dir);
   make_makefile_lum($makefile_lum_src, $makefile_lum_dst, $makefile_lum_tmp);
-  make_makefile_prj($makefile_prj_src, $makefile_prj_dst, $ev3rt_dir);
+  make_makefile_prj($makefile_prj_src, $makefile_prj_dst, $obj_dir, $ev3rt_dir);
   make_cfgfile($cfgfile_src, $cfgfile_dst, \@c_objs, \@cpp_objs);
 }
 
